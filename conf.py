43901b937822bed4c71a44a78df2a75f95c855a9
Viraaj SIngh Gill <125380557+viraajsinghgill@users.noreply.github.com>
2023-04-16 21:46:57 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,10 +1,10 @@
-MAILGUN_API_KEY='98d854f15378351c89c37b8ae3e1b8f9-15b35dee-cd136286'
-SANDBOX_URL='sandbox2aa52ceee3f9406ab96668c4247fa99a.mailgun.org'
+MAILGUN_API_KEY='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SANDBOX_URL='sandbox2aa52cexxxxxxxxxxxxxxxxxx.mailgun.org'
 SENDER_EMAIL='test@'+SANDBOX_URL
-RECIPIENT_EMAIL='vinny.gill2@gmail.com'
-bolt_api_key='b59f4465-c779-4575-8def-23c8342ebc27'
-device_id='BOLT14486397'
+RECIPIENT_EMAIL='xxxxxxxx@gmail.com'
+bolt_api_key='bxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+device_id='BOLTxxxxxxxx7'
 
 telegram_chat_id="@temperature_alert_vinny"
-telegram_bot_id="bot6097264625:AAHz-OAYJEXQRFVK9QhgKSbnhwT3Sxn49ig"
+telegram_bot_id="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 threshold=240
